f381542b69a8d19068f8f808b406843792755023
AmaroCesa <amro_txt@hotmail.com>
2019-07-20 21:15:51 -0300

    wsgi

--- intmed_test/intmed_test/wsgi.py
+++ intmed_test/intmed_test/wsgi.py
@@ -1,16 +1,18 @@
 """
-WSGI config for intmed_test project.
+WSGI config for Loja project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
 import os
+import sys
 
+path = os.path.expanduser('~/Intmed/intmed_test')
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'intmed_test.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intmed_test.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
