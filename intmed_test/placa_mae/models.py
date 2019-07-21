c26b95bb6289bce0a087d357c157d38a4f774df8
AmaroCesa <amro_txt@hotmail.com>
2019-07-20 21:25:17 -0300

    field incorreto

--- intmed_test/placa_mae/models.py
+++ intmed_test/placa_mae/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from produto.models import Produto
-
+from processador.models import Processador
 # Create your models here.
 class PlacaMae(models.Model):
     nome = models.CharField(max_length=30)
     produto = models.ForeignKey(Produto, on_delete=models.PROTECT)
-    processador = models.ForeignKey(Processador)
+    processador = models.ForeignKey(Processador,  on_delete=models.PROTECT)
     qtd_memoria = models.PositiveSmallIntegerField()
     qtd_ram = models.PositiveSmallIntegerField()
     video_integrado = models.BooleanField()
