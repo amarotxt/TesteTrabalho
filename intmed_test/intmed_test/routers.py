6ad02a8e5ffc07834abd90770be928bb6a82000c
AmaroCesa <amro_txt@hotmail.com>
2019-07-20 21:43:27 -0300

    definicao variavel

--- intmed_test/intmed_test/routers.py
+++ intmed_test/intmed_test/routers.py
@@ -1,9 +1,9 @@
 from rest_framework import routers
 from produto.views import ProdutoViewSet
 from memoria_ram.views import MemoriaRamViewSet
-from produto.views import PlacaMaeViewSet
-from produto.views import PlacaVideoViewSet
-from produto.views import ProcessadorViewSet
+from placa_mae.views import PlacaMaeViewSet
+from placa_video.views import PlacaVideoViewSet
+from processador.views import ProcessadorViewSet
 
 router = routers.DefaultRouter()
 router.register('memoriaram',MemoriaRamViewSet, base_name='memoriaram')
