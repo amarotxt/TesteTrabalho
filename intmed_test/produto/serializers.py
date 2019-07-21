96a91457c3e4c00e5f7d32a3f2bc8dfe481f0f0a
AmaroCesa <amro_txt@hotmail.com>
2019-07-20 21:29:16 -0300

    serializer corrigido

--- intmed_test/produto/serializers.py
+++ intmed_test/produto/serializers.py
@@ -1,6 +1,5 @@
 from .models import Produto
 from rest_framework import serializers
-from pedido.serializer import PedidoSerializer
 
 class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
