f1e135a419aea9fa5ab4f5e545f7143a372ee1d3
AmaroCesa <amro_txt@hotmail.com>
2019-07-20 21:26:44 -0300

    field incorreto

--- intmed_test/placa_video/models.py
+++ intmed_test/placa_video/models.py
@@ -3,5 +3,5 @@ from produto.models import Produto
 
 # Create your models here.
 class PlacaVideo(models.Model):
-    produto = models.ForeignKey(Produto)
+    produto = models.ForeignKey(Produto, on_delete=models.PROTECT)
     nome = models.CharField(max_length=30)
