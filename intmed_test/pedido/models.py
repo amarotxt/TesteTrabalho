48a6579e7c432b2d665f03caeed09d61ab8977cd
AmaroCesa <amro_txt@hotmail.com>
2019-07-23 21:40:14 -0300

    corrigir model foreng

--- intmed_test/pedido/models.py
+++ intmed_test/pedido/models.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Pedido(models.Model):
-    placa_mae = models.ForeingKey(PlacaMae, on_delete=models.PROTECT) 
-    placa_video = models.ForeingKey(PlacaVideo, on_delete=models.PROTECT) 
-    processador = models.ForeingKey(Processador, on_delete=models.PROTECT) 
+    placa_mae = models.ForeignKey(PlacaMae, on_delete=models.PROTECT) 
+    placa_video = models.ForeignKey(PlacaVideo, on_delete=models.PROTECT) 
+    processador = models.ForeignKey(Processador, on_delete=models.PROTECT) 
     memoria_ram = models.ManyToManyField(MemoriaRam, on_delete=models.PROTECT) 
-    cliente = models.ForeingKey(User, on_delete=models.PROTECT)
+    cliente = models.ForeignKey(User, on_delete=models.PROTECT)
