dc01bb30e03141c2a253adf634960f8cfe52da10
AmaroCesa <amro_txt@hotmail.com>
2019-07-23 21:58:06 -0300

    url

--- intmed_test/intmed_test/urls.py
+++ intmed_test/intmed_test/urls.py
@@ -19,6 +19,6 @@ from .routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('^api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
 ]
