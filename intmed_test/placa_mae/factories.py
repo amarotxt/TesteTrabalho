3bca1d37692e2504cad6b20cb63ee5b2ddad4db0
AmaroCesa <amro_txt@hotmail.com>
2019-08-11 19:47:05 -0300

    teste

--- intmed_test/placa_mae/factories.py
+++ intmed_test/placa_mae/factories.py
@@ -9,9 +9,9 @@ class PlacaMaeFactory():
     def create_placa_mae(self):
         placa_mae = mommy.make(
             PlacaMae,
-            processador=ProcessadorFactory().create_processador(),
+            processador=[ProcessadorFactory().create_processador()],
             qtd_memoria_ram_slots=random.choices([2,4])[0],
             qtd_memoria_ram_total=random.choices([16,64])[0],
             produto=ProdutoFactory().create_produto(),
-            make_m2m=True )
+            )
         return placa_mae
