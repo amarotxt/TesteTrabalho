af25b607bb2e4e3e58633f73ccf6676ce35f665c
AmaroCesa <amro_txt@hotmail.com>
2019-08-11 19:54:00 -0300

    teste

--- intmed_test/pedido/views.py
+++ intmed_test/pedido/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import PedidoSerializer
+from .models import Pedido
 # Create your views here.
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer 
+
