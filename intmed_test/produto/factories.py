113213819ca354da38b0ee7477ed29587628e22b
AmaroCesa <amro_txt@hotmail.com>
2019-08-11 19:17:20 -0300

    teste

--- intmed_test/produto/factories.py
+++ intmed_test/produto/factories.py
@@ -4,5 +4,6 @@ from model_mommy import mommy
 class ProdutoFactory():
     def create_produto(self):
         produto = mommy.make(Produto)
+        import ipdb; ipdb.set_trace()
         return produto
 
