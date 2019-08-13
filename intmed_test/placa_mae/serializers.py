8f238644b65b8ffbd115814e661c822920481706
AmaroCesa <amro_txt@hotmail.com>
2019-08-12 23:22:05 -0300

    pedido

--- intmed_test/placa_mae/serializers.py
+++ intmed_test/placa_mae/serializers.py
@@ -1,5 +1,6 @@
 from .models import PlacaMae
 from rest_framework import serializers
+from memoria_ram.models import MemoriaRam
 
 class PlacaMaeSerializer(serializers.ModelSerializer):
     memoria_ram = serializers.PrimaryKeyRelatedField(queryset=MemoriaRam.objects.all() ,many=True)
