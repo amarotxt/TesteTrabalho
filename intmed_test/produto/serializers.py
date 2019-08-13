a58c3a063d0ea24179d1008ca84e16880324c543
AmaroCesa <amro_txt@hotmail.com>
2019-08-13 07:23:46 -0300

    pedido

--- intmed_test/produto/serializers.py
+++ intmed_test/produto/serializers.py
@@ -1,9 +1,7 @@
 from .models import Produto
 from rest_framework import serializers
-from produto.serializers import ProdutoSerializer
 
 class ProdutoSerializer(serializers.ModelSerializer):
-    produto = ProdutoSerializer()
     class Meta:
         model = Produto
         fields = ('__all__')
