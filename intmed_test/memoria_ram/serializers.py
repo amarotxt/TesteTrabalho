0feb74a66ea32290e9ceba6007ed0212fe889f10
AmaroCesa <amro_txt@hotmail.com>
2019-08-13 00:32:51 -0300

    pedido

--- intmed_test/memoria_ram/serializers.py
+++ intmed_test/memoria_ram/serializers.py
@@ -1,10 +1,16 @@
 from .models import MemoriaRam
 from rest_framework import serializers
-
+from produto import Produto
 class MemoriaRamSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = MemoriaRam
-        fields = ('__all__')
+        fields = ('nome', 'tamanho')
+        extra_kwargs = {
+            'nome': {'allow_null': False, 'required': True},
+            'tamanho': {'allow_null': False,'required': True,},     
+        }
+
 
 
 
--- intmed_test/memoria_ram/views.py
+++ intmed_test/memoria_ram/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import serializers
 from .serializers import MemoriaRamSerializer
 from .models import MemoriaRam
 # Create your views here.
@@ -8,3 +9,8 @@ class MemoriaRamViewSet(viewsets.ModelViewSet):
     queryset = MemoriaRam.objects.all()
     serializer_class = MemoriaRamSerializer 
 
+    def create(self, validated_data):
+        serializer = self.get_serializer(data=self.request.data)
+        if not serializer.is_valid():
+            raise serializers.ValidationError()
+        
