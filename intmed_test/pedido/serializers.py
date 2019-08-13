fef37a6e5d166d83c7962cabfc7598c467754072
AmaroCesa <amro_txt@hotmail.com>
2019-08-12 22:25:12 -0300

    pedido

--- intmed_test/pedido/serializers.py
+++ intmed_test/pedido/serializers.py
@@ -19,7 +19,7 @@ from rest_framework.exceptions import ValidationError
 
 def validar_memoria_ram(instance):
     
-    memorias_rams = instance.memoria_ram    
+    memorias_rams = instance['memoria_ram']    
     placa_mae = PlacaMae.objects.filter(processador=instace.placa_mae.id).first()
     if memoria_ram is not None  and placa_mae is not None:
         count_memoria_ram = memorias_rams.count()
@@ -34,7 +34,7 @@ def validar_memoria_ram(instance):
 
 def validar_placa_mae(instance):
     import ipdb; ipdb.set_trace()
-    processador = instace ['placa_mae']
+    processador = instance['placa_mae']
     placa_mae = PlacaMae.objects.filter(processador=instace.placa_mae.id).first()
     if processador is not None and placa_mae is not None:
         placa_mae_processadores_list = placa_mae.processador.all()
@@ -48,7 +48,7 @@ def validar_placa_mae(instance):
 
 def validar_placa_video(instance):
 
-    placa_mae =instace ['placa_mae']
+    placa_mae = instance['placa_mae']
     placa_videos = PlacaVideo.objects.filter(processador=instace.placa_video.id).first()
     if placa_mae is not None:
         if not placa_mae.video_integrado:
