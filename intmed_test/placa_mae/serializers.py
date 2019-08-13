4f2831c64b6f8b95e87332c1f030fa1b7b5eb6b9
AmaroCesa <amro_txt@hotmail.com>
2019-08-12 23:31:27 -0300

    pedido

--- intmed_test/placa_mae/serializers.py
+++ intmed_test/placa_mae/serializers.py
@@ -1,9 +1,9 @@
 from .models import PlacaMae
 from rest_framework import serializers
-from memoria_ram.models import MemoriaRam
+from Processador.models import Processador
 
 class PlacaMaeSerializer(serializers.ModelSerializer):
-    memoria_ram = serializers.PrimaryKeyRelatedField(queryset=MemoriaRam.objects.all() ,many=True)
+    processador = serializers.PrimaryKeyRelatedField(queryset=Processador.objects.all() ,many=True)
     class Meta:
         model = PlacaMae
         fields = ('__all__')
