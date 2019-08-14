814b1b94fd49bfabed717de1c12d72235059bb0c
AmaroCesa <amro_txt@hotmail.com>
2019-08-13 22:24:34 -0300

    pedido

--- intmed_test/memoria_ram/serializers.py
+++ intmed_test/memoria_ram/serializers.py
@@ -3,16 +3,16 @@ from rest_framework import serializers
 from produto.serializers import ProdutoSerializer
 
 class MemoriaRamSerializer(serializers.ModelSerializer):
-    produto = ProdutoSerializer()
+    produto = ProdutoSerializer(read_only=True)
     class Meta:
         model = MemoriaRam
-        fields = ('nome', 'tamanho', 'produto')
+        fields = ('__all__')
         # extra_kwargs = {
         #     'nome': {'allow_null': False, 'required': True},
         #     'tamanho': {'allow_null': False,'required': True,},     
         # }
-    
-  
 
+        def create(self, validated_data):
+            raise Exception(validated_data)
 
 
--- intmed_test/processador/serializers.py
+++ intmed_test/processador/serializers.py
@@ -3,14 +3,9 @@ from rest_framework import serializers
 from produto.serializers import ProdutoSerializer
 
 class ProcessadorSerializer(serializers.ModelSerializer):
-    produto = ProdutoSerializer(read_only=True)
+    produto = ProdutoSerializer()
     class Meta:
         model = Processador
         fields = ('__all__')
 
-    def create(self, validated_data):
-        raise Exception(validated_data)
-        
-    # def update(isntance, validated_data):
-    #     pass
 
