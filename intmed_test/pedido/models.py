e6fe9cdd079205fe0180867625cfa74d390a9911
AmaroCesa <amro_txt@hotmail.com>
2019-08-13 21:19:15 -0300

    pedido

--- intmed_test/pedido/models.py
+++ intmed_test/pedido/models.py
@@ -6,7 +6,7 @@ from memoria_ram.models import MemoriaRam
 from django.contrib.auth.models import User
 
 class PedidoMemoriaRam(models.Model):
-    memodira_ram = models.ForeignKey(MemoriaRam)
+    memodira_ram = models.ForeignKey(MemoriaRam, on_delete=models.PROTECT)
     quantidade = mdoels.PositiveSmallIntegerField()
     
 # Create your models here.
@@ -14,6 +14,6 @@ class Pedido(models.Model):
     placa_mae = models.ForeignKey(PlacaMae, on_delete=models.PROTECT) 
     placa_video = models.ForeignKey(PlacaVideo, on_delete=models.PROTECT) 
     processador = models.ForeignKey(Processador, on_delete=models.PROTECT) 
-    memoria_ram = models.ForeignKey(PedidoMemoriaRam) 
+    memoria_ram = models.ForeignKey(PedidoMemoriaRam, on_delete=models.PROTECT) 
     cliente = models.ForeignKey(User, on_delete=models.PROTECT)
 
