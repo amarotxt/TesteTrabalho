f24b5208f4293a34dfc9296f4be384adcc1bf452
AmaroCesa <amro_txt@hotmail.com>
2019-08-14 00:23:26 -0300

    pedido

--- intmed_test/intmed_test/management/commands/populate_db.py
+++ intmed_test/intmed_test/management/commands/populate_db.py
@@ -1,18 +1,16 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand, CommandError,no_translations
 from memoria_ram.factories import MemoriaRamFactory
 from processador.factories import ProcessadorFactory
 from placa_mae.factories import PlacaMaeFactory
 from placa_video.factories import PlacaVideoFactory
 
 class Command(BaseCommand):
-    help = 'Seeds the database.'
-    def create_class(self):
+    help = 'Seeds the database
+    @no_translations
+    def handle(self, *args, **options):
         for i in range(2):
             ProcessadorFactory().create_processador()
             PlacaMaeFactory().create_placa_mae()
             PlacaVideoFactory().create_placa_videos()
             MemoriaRamFactory().create_placa_videos()
             
-
-    def handle(self, *args, **options):
-        self.create_class()
