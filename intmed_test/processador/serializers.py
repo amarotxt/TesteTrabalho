54b9fd05257d4f2fdf5db5e764e7e262b5a2f4b0
AmaroCesa <amro_txt@hotmail.com>
2019-08-13 22:13:46 -0300

    pedido

--- intmed_test/processador/serializers.py
+++ intmed_test/processador/serializers.py
@@ -3,9 +3,14 @@ from rest_framework import serializers
 from produto.serializers import ProdutoSerializer
 
 class ProcessadorSerializer(serializers.ModelSerializer):
-    produto = ProdutoSerializer()
+    produto = ProdutoSerializer(read_only=True)
     class Meta:
         model = Processador
         fields = ('__all__')
 
+    # def create(isntance, validated_data):
+    #     pass
+    
+    # def update(isntance, validated_data):
+    #     pass
 
