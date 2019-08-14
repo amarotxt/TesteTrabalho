6db66f195127f950f91426d665df6d2aa8757585
AmaroCesa <amro_txt@hotmail.com>
2019-08-13 23:03:57 -0300

    pedido

--- intmed_test/memoria_ram/views.py
+++ intmed_test/memoria_ram/views.py
@@ -4,14 +4,17 @@ from rest_framework import serializers
 from .serializers import MemoriaRamSerializer
 from .models import MemoriaRam
 from produto.models import Produto
+
+from rest_framework.response import Response
 # Create your views here.
 
 class MemoriaRamViewSet(viewsets.ModelViewSet):
     queryset = MemoriaRam.objects.all()
     serializer_class = MemoriaRamSerializer 
 
-    # def create(self, validated_data):
-    #     serializer = self.get_serializer(data=self.request.data)
-    #     if not serializer.is_valid():
-    #         raise serializers.ValidationError()
-    #     Produto.objects.create(fornecedor='Atu')
+    def create(self, request, *args, **kwargs):
+        response = super(MemoriaRamViewSet, self).create(request, *args, **kwargs)
+        if response.status == 201:
+            produto = Produto.objects.create()
+            memoria = Memoriaram.objects.get(response.data)
+        return response
