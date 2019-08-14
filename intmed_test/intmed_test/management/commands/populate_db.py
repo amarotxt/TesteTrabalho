a3c8a05dbfd14c46d294dd0c8a4248ddaf4ca8a5
AmaroCesa <amro_txt@hotmail.com>
2019-08-14 00:14:16 -0300

    pedido

---
+++ intmed_test/intmed_test/management/commands/populate_db.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from memoria_ram.factories import MemoriaRamFactory
+from processador.factories import ProcessadorFactory
+from placa_mae.factories import PlacaMaeFactory
+from placa_video.factories import PlacaVideoFactory
+
+class Command(BaseCommand):
+    def create_class(self):
+        for i in range(2):
+            ProcessadorFactory().create_processador()
+            PlacaMaeFactory().create_placa_mae()
+            PlacaVideoFactory().create_placa_videos()
+            MemoriaRamFactory().create_placa_videos()
+            
+
+    def handle(self, *args, **options):
+        self.create_tags()
